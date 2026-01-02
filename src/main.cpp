7465babe1f84a40480a17a61eab88c38ea8166b5
George Vigelette <gvigelet@duvitech.com>
2026-01-01 20:59:34 -0500

    working display

--- src/main.cpp
+++ src/main.cpp
@@ -1,281 +1,152 @@
 // For a connection via I2C using the Arduino Wire include:
-#include <Wire.h>
-#include <SPI.h>
+#include <Wire.h>               
 #include "HT_SSD1306Wire.h"
-#include <RadioLib.h>
+#include "images.h"
+void VextON(void);
+void VextOFF(void);
+static SSD1306Wire  display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
 
-// MODE SELECTION: Set to true for transmitter, false for receiver
-#define IS_TRANSMITTER true
+#define DEMO_DURATION 3000
+typedef void (*Demo)(void);
 
-// Create display instance using board-specific pin definitions from pins_arduino.h
-// For WiFi LoRa 32 V3: SDA_OLED=17, SCL_OLED=18, RST_OLED=21
+int demoMode = 0;
+int counter = 1;
 
-static SSD1306Wire display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED);
-
-#define RF_FREQUENCY 915.0      // MHz
-#define TX_OUTPUT_POWER 14      // dBm
-#define LORA_BANDWIDTH 125.0    // kHz
-#define LORA_SPREADING_FACTOR 7 // [SF7..SF12]
-#define LORA_CODINGRATE 5       // [5: 4/5, 6: 4/6, 7: 4/7, 8: 4/8]
-#define LORA_PREAMBLE_LENGTH 8  // Same for Tx and Rx
-
-#define BUFFER_SIZE 30 // Define the payload size here
-
-// Pin definitions for Heltec WiFi LoRa 32 V3
-#define RADIO_SCLK_PIN 9
-#define RADIO_MISO_PIN 11
-#define RADIO_MOSI_PIN 10
-#define RADIO_CS_PIN 8
-#define RADIO_DIO1_PIN 14
-#define RADIO_RST_PIN 12
-#define RADIO_BUSY_PIN 13
-#define LORA_PWR_PIN 36 // LoRa power enable
-
-// Create radio instance (SX1262 for V3 board)
-SX1262 radio = new Module(RADIO_CS_PIN, RADIO_DIO1_PIN, RADIO_RST_PIN, RADIO_BUSY_PIN);
-
-int counter = 0;
-char rxpacket[BUFFER_SIZE];
-int16_t rssi_value, snr_value;
-bool lora_idle = true;
-static uint32_t loop_count = 0;
-
-// Flag to indicate that a packet was received
-volatile bool receivedFlag = false;
-static int state;
-
-// This function is called when a complete packet is received by the module
-void setFlag(void)
-{
-  receivedFlag = true;
-}
-
-void setup()
-{
-  // Initialize Serial
+void setup() {
   Serial.begin(115200);
-
-  // Turn on Vext power for display
-  pinMode(Vext, OUTPUT);
-  digitalWrite(Vext, LOW); // Vext is active LOW
+  Serial.println();
+  Serial.println();
+  VextON();
   delay(100);
 
-  // Initialize display BEFORE SPI (display uses I2C)
   // Initialising the UI will init the display too.
   display.init();
 
-  delay(100);
+  display.setFont(ArialMT_Plain_10);
 
-  // Turn on LoRa radio power
-  pinMode(LORA_PWR_PIN, OUTPUT);
-  digitalWrite(LORA_PWR_PIN, HIGH); // Enable LoRa power
-  delay(100);
-
-  // Initialize SPI for LoRa radio AFTER display
-  SPI.begin(RADIO_SCLK_PIN, RADIO_MISO_PIN, RADIO_MOSI_PIN, RADIO_CS_PIN);
-
-  Serial.print(F("[SX1262] Initializing ... "));
-
-  // Initialize SX1262 with default settings first
-  state = radio.begin();
-
-  if (state == RADIOLIB_ERR_NONE)
-  {
-    Serial.println(F("success!"));
-
-    // Now configure the radio
-    state = radio.setFrequency(RF_FREQUENCY);
-    if (state != RADIOLIB_ERR_NONE)
-    {
-      Serial.print(F("Failed to set frequency, code "));
-      Serial.println(state);
-    }
-
-    state = radio.setBandwidth(LORA_BANDWIDTH);
-    if (state != RADIOLIB_ERR_NONE)
-    {
-      Serial.print(F("Failed to set bandwidth, code "));
-      Serial.println(state);
-    }
-
-    state = radio.setSpreadingFactor(LORA_SPREADING_FACTOR);
-    if (state != RADIOLIB_ERR_NONE)
-    {
-      Serial.print(F("Failed to set spreading factor, code "));
-      Serial.println(state);
-    }
-
-    state = radio.setCodingRate(LORA_CODINGRATE);
-    if (state != RADIOLIB_ERR_NONE)
-    {
-      Serial.print(F("Failed to set coding rate, code "));
-      Serial.println(state);
-    }
-
-    state = radio.setOutputPower(TX_OUTPUT_POWER);
-    if (state != RADIOLIB_ERR_NONE)
-    {
-      Serial.print(F("Failed to set output power, code "));
-      Serial.println(state);
-    }
-
-    state = radio.setPreambleLength(LORA_PREAMBLE_LENGTH);
-    if (state != RADIOLIB_ERR_NONE)
-    {
-      Serial.print(F("Failed to set preamble length, code "));
-      Serial.println(state);
-    }
-  }
-  else
-  {
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-    display.clear();
-    display.drawString(0, 0, "Radio Init FAIL");
-    display.drawString(0, 20, "Code: " + String(state));
-    display.display();
-    while (true)
-    {
-      delay(10);
-    }
-  }
-
-  Serial.println("ESP32 initialized");
-  loop_count = 0;
 }
 
-void loop()
-{
-  if (loop_count == 0)
-  {
-    loop_count = 1;
+void drawFontFaceDemo() {
+    // Font Demo1
+    // create more fonts at http://oleddisplay.squix.ch/
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 0, "Hello world");
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(0, 10, "Hello world");
+    display.setFont(ArialMT_Plain_24);
+    display.drawString(0, 26, "Hello world");
+}
+
+void drawTextFlowDemo() {
     display.setFont(ArialMT_Plain_10);
     display.setTextAlignment(TEXT_ALIGN_LEFT);
-    display.clear();
+    display.drawStringMaxWidth(0, 0, 128,
+      "Lorem ipsum\n dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore." );
+}
 
-    // Display initialization message
-    display.drawString(0, 0, "Heltec WiFi LoRa");
-    display.drawString(0, 20, "Radio Init OK");
-#if IS_TRANSMITTER
-    // Transmitter mode
-    Serial.println("Mode: TRANSMITTER");
-    display.drawString(0, 40, "Mode: TX");
+void drawTextAlignmentDemo() {
+  // Text alignment demo
+  char str[30];
+  int x = 0;
+  int y = 0;
+  display.setFont(ArialMT_Plain_10);
+  // The coordinates define the left starting point of the text
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.drawString(x, y, "Left aligned (0,0)");
 
-#else
-    // Receiver mode
-    Serial.println("Mode: RECEIVER");
-    display.drawString(0, 40, "Mode: RX");
+  // The coordinates define the center of the text
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  x = display.width()/2;
+  y = display.height()/2-5;
+  sprintf(str,"Center aligned (%d,%d)",x,y);
+  display.drawString(x, y, str);
 
-    // Set the function that will be called when a packet is received
-    radio.setDio1Action(setFlag);
+  // The coordinates define the right end of the text
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  x = display.width();
+  y = display.height()-12;
+  sprintf(str,"Right aligned (%d,%d)",x,y);
+  display.drawString(x, y, str);
+}
 
-    // Start listening for LoRa packets
-    Serial.print(F("[SX1262] Starting to listen ... "));
-    state = radio.startReceive();
-    if (state == RADIOLIB_ERR_NONE)
-    {
-      Serial.println(F("success!"));
+void drawRectDemo() {
+      // Draw a pixel at given position
+    for (int i = 0; i < 10; i++) {
+      display.setPixel(i, i);
+      display.setPixel(10 - i, i);
     }
-    else
-    {
-      Serial.print(F("failed, code "));
-      Serial.println(state);
-      while (true)
-        ;
+    display.drawRect(12, 12, 20, 20);
+
+    // Fill the rectangle
+    display.fillRect(14, 14, 17, 17);
+
+    // Draw a line horizontally
+    display.drawHorizontalLine(0, 40, 20);
+
+    // Draw a line horizontally
+    display.drawVerticalLine(40, 0, 20);
+}
+
+void drawCircleDemo() {
+  for (int i=1; i < 8; i++) {
+    display.setColor(WHITE);
+    display.drawCircle(32, 32, i*3);
+    if (i % 2 == 0) {
+      display.setColor(BLACK);
     }
-
-#endif
-
-    display.display();
-    delay(1000); // Show mode briefly
+    display.fillCircle(96, 32, 32 - i* 3);
   }
-  else
-  {
+}
 
-#if IS_TRANSMITTER
-    // Transmitter mode - send packets periodically
-    Serial.print(F("[SX1262] Transmitting packet ... "));
+void drawProgressBarDemo() {
+  int progress = (counter / 5) % 100;
+  // draw the progress bar
+  display.drawProgressBar(0, 32, 120, 10, progress);
 
-    // Prepare packet
-    String message = "Hello World! #" + String(counter++);
+  // draw the percentage as String
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(64, 15, String(progress) + "%");
+}
 
-    // Update display
-    display.clear();
-    display.drawString(0, 0, "Transmitting:");
-    display.drawString(0, 20, message.substring(0, 20));
-    display.drawString(0, 40, "Count: " + String(counter));
-    display.display();
+void drawImageDemo() {
+    // see http://blog.squix.org/2015/05/esp8266-nodemcu-how-to-create-xbm.html
+    // on how to create xbm files
+    display.drawXbm(34, 14, WiFi_Logo_width, WiFi_Logo_height, WiFi_Logo_bits);
+}
 
-    // Transmit packet
-    int state = radio.transmit(message);
+void VextON(void)
+{
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext, LOW);
+}
 
-    if (state == RADIOLIB_ERR_NONE)
-    {
-      Serial.println(F("success!"));
-      Serial.print(F("[SX1262] Data:\t\t"));
-      Serial.println(message);
-    }
-    else
-    {
-      Serial.print(F("failed, code "));
-      Serial.println(state);
-    }
+void VextOFF(void) //Vext default OFF
+{
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext, HIGH);
+}
 
-    // Wait before next transmission
-    delay(2000);
 
-#else
-    // Receiver mode - check for incoming packets
-    // Check if the flag is set
-    if (receivedFlag)
-    {
-      // Reset flag
-      receivedFlag = false;
+Demo demos[] = {drawFontFaceDemo, drawTextFlowDemo, drawTextAlignmentDemo, drawRectDemo, drawCircleDemo, drawProgressBarDemo, drawImageDemo};
+int demoLength = (sizeof(demos) / sizeof(Demo));
+long timeSinceLastModeSwitch = 0;
 
-      // Read received packet
-      String str;
-      int state = radio.readData(str);
+void loop() {
+  // clear the display
+  display.clear();
+  // draw the current demo method
+  demos[demoMode]();
 
-      if (state == RADIOLIB_ERR_NONE)
-      {
-        // Packet received successfully
-        Serial.println(F("[SX1262] Received packet!"));
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  display.drawString(10, 128, String(millis()));
+  // write the buffer to the display
+  display.display();
 
-        // Get RSSI and SNR
-        rssi_value = radio.getRSSI();
-        snr_value = radio.getSNR();
-
-        // Print data
-        Serial.print(F("[SX1262] Data:\t\t"));
-        Serial.println(str);
-        Serial.print(F("[SX1262] RSSI:\t\t"));
-        Serial.print(rssi_value);
-        Serial.println(F(" dBm"));
-        Serial.print(F("[SX1262] SNR:\t\t"));
-        Serial.print(snr_value);
-        Serial.println(F(" dB"));
-
-        // Update display
-        display.clear();
-        display.drawString(0, 0, "Received:");
-        display.drawString(0, 20, str.substring(0, 20));
-        display.drawString(0, 35, "RSSI: " + String(rssi_value) + " dBm");
-        display.drawString(0, 50, "SNR: " + String(snr_value) + " dB");
-        display.display();
-      }
-      else if (state == RADIOLIB_ERR_CRC_MISMATCH)
-      {
-        Serial.println(F("[SX1262] CRC error!"));
-      }
-      else
-      {
-        Serial.print(F("[SX1262] Failed, code "));
-        Serial.println(state);
-      }
-
-      // Put module back to listen mode
-      radio.startReceive();
-    }
-#endif
+  if (millis() - timeSinceLastModeSwitch > DEMO_DURATION) {
+    demoMode = (demoMode + 1)  % demoLength;
+    timeSinceLastModeSwitch = millis();
   }
+  counter++;
+  delay(10);
 }
